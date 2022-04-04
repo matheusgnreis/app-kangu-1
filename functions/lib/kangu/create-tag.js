90787ba3d197e22393c71e0b713190904146f81c
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-04-04 17:46:48 -0300

    feat: add create tag without some info

--- functions/lib/kangu/create-tag.js
+++ functions/lib/kangu/create-tag.js
@@ -59,7 +59,12 @@ module.exports = (token, order) => {
       }
     })
   }
+  data.origem = 'E-Com Plus'
 
+  data.pedido = {
+    numeroCli: order._id,
+    vlrMerc: order.amount && order.amount.total || 0
+  }
   
 
   const buyer = order.buyers && order.buyers[0]
@@ -71,51 +76,47 @@ module.exports = (token, order) => {
   if (order.shipping_lines) {
     order.shipping_lines.forEach(shippingLine => {
       if (shippingLine.app) {
-        // check for valid Correios service
-        data.forma_envio = shippingLine.app.service_name
-        switch (data.forma_envio) {
-          case 'PAC':
-          case 'SEDEX':
-          case 'PACMINI':
-            // parse addresses and package info from shipping line object
-            data.destinatario = shippingLine.to.name
-            data.cep = shippingLine.to.zip.replace(/\D/g, '')
-            data.logradouro = shippingLine.to.street
-            data.bairro = shippingLine.to.borough
-            data.numero = shippingLine.to.number || 'SN'
-            if (shippingLine.to.complement) {
-              data.complemento = shippingLine.to.complement
-            }
-            data.cidade = shippingLine.to.city
-            data.estado = shippingLine.to.province_code
-            if (shippingLine.package && shippingLine.package.weight) {
-              const { value, unit } = shippingLine.package.weight
-              data.peso = !unit || unit === 'kg' ? value
-                : unit === 'g' ? value * 1000
-                  : value * 1000000
-              data.altura = 2
-              data.largura = 11
-              data.comprimento = 16
-            }
-            if (shippingLine.declared_value) {
-              data.valor_seguro = shippingLine.declared_value
-            }
-            data.cep_origem = shippingLine.from.zip.replace(/\D/g, '')
-            console.log(`> Create tag for #${order._id}: ` + JSON.stringify(data))
-            // send POST to generate Manda Bem tag
-            requests.push(axios.post(
-              'https://mandabem.com.br/ws/gerar_envio',
-              qs.stringify(data),
-              {
-                headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded'
-                }
-              }
-            ).then(response => {
-              console.log('> Manda Bem create tag', JSON.stringify(response.data))
-              return response
-            }).catch(console.error))
+        data.servicos = [shippingLine.service_code]
+        // parse addresses and package info from shipping line object
+        if (shippingLine.from) {
+          data.remetente = {
+            
+          }
         }
+        data.destinatario = shippingLine.to.name
+        data.cep = shippingLine.to.zip.replace(/\D/g, '')
+        data.logradouro = shippingLine.to.street
+        data.bairro = shippingLine.to.borough
+        data.numero = shippingLine.to.number || 'SN'
+        if (shippingLine.to.complement) {
+          data.complemento = shippingLine.to.complement
+        }
+        data.cidade = shippingLine.to.city
+        data.estado = shippingLine.to.province_code
+        if (shippingLine.package && shippingLine.package.weight) {
+          const { value, unit } = shippingLine.package.weight
+          data.pedido.pesoMerc = !unit || unit === 'kg' ? value
+            : unit === 'g' ? value * 1000
+              : value * 1000000
+        }
+        if (shippingLine.declared_value) {
+          data.valor_seguro = shippingLine.declared_value
+        }
+        data.cep_origem = shippingLine.from.zip.replace(/\D/g, '')
+        console.log(`> Create tag for #${order._id}: ` + JSON.stringify(data))
+        // send POST to generate Manda Bem tag
+        requests.push(axios.post(
+          'https://mandabem.com.br/ws/gerar_envio',
+          qs.stringify(data),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+        ).then(response => {
+          console.log('> Manda Bem create tag', JSON.stringify(response.data))
+          return response
+        }).catch(console.error))    
       }
     })
   }
